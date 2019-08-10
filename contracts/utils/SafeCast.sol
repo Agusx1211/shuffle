58b8a6b24b12764fa27dcd3f916faf9ab69c3e08
Agustin Aguilar <agusxrun@gmail.com>
2019-08-10 21:05:43 +0200

    Remove commented code safecast

--- contracts/utils/SafeCast.sol
+++ contracts/utils/SafeCast.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.5.10;
 library SafeCast {
     function toUint96(uint256 _a) internal pure returns (uint96) {
         require(_a <= 2 ** 96 - 1, "cast uint96 overflow");
-        // assembly { _a := and(_a, sub(exp(2, 96), 1)) }
         return uint96(_a);
     }
 }
